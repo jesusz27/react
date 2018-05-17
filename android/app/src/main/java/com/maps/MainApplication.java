fdeb625c7d26cb689d37b7f30901483185bac3cd
jesusz27 <jash.zea@gmail.com>
2018-05-17 19:08:09 +0200

    background

--- android/app/src/main/java/com/maps/MainApplication.java
+++ android/app/src/main/java/com/maps/MainApplication.java
@@ -3,7 +3,9 @@ package com.maps;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+
 import com.airbnb.android.react.maps.MapsPackage;
+import com.marianhello.bgloc.react.BackgroundGeolocationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage()
+            new MapsPackage(),
+			new BackgroundGeolocationPackage()
       );
     }
 
