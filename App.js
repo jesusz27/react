71c7d290726eda047497e0479544914d0e51f755
jesusz27 <jash.zea@gmail.com>
2018-05-17 15:19:36 +0200

    wacth position

--- App.js
+++ App.js
@@ -5,11 +5,30 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View, Text} from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+import { PermissionsAndroid } from 'react-native';
 
 
 export default class App extends Component {
+
+
+  componentWillMount(){
+    this.watchId = navigator.geolocation.watchPosition(
+      (position) => {
+       
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          i: this.state.i+1,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    );
+
+  }
+
 
   state = {
     region: {
@@ -18,13 +37,17 @@ export default class App extends Component {
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     },
+    latitude:null,
+    longitude:null,
+    i:0
   };
   render() {
     return (
-      <MapView
-        style={styles.map}
-        region={this.state.region}
-      />
+      
+      <View>
+      <Text>{this.state.latitude}--{this.state.i}</Text>
+    </View>
+
     );
   }
 }
